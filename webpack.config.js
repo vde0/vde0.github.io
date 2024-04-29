9f7ecc6cf331cf9cfcc6dc57a07a2acebb05e012
P-PRINTER <psevdoprinter@gmail.com>
2024-04-30 00:49:48 +0200

    configure project with webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            { test: /\.(js)$/, use: 'babel-loader' },
+        ]
+    },
+    devServer: {
+        static: {
+            directory: __dirname + '/docs/'
+        },
+        compress: true,
+        port: 9000
+    },
+    output: {
+        path: __dirname + '/docs/',
+        filename: 'index_bundle.js'
+    },
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+};
