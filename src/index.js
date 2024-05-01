304a9374b7d60109425db04b85217c711b251672
P-PRINTER <psevdoprinter@gmail.com>
2024-05-01 13:21:26 +0200

    make app window expanded for tg

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import './index.css'
 
 const tg = window.Telegram.WebApp;
+tg.expand();
 
 const root = createRoot( document.getElementById("root") );
 root.render(<App telegram={tg} />);
