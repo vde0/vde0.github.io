721b42b7838d0be3d409957bffe2fdb28b716ddc
P-PRINTER <psevdoprinter@gmail.com>
2024-05-01 13:25:13 +0200

    add an expanded check

--- src/App.js
+++ src/App.js
@@ -59,6 +59,7 @@ export default class App extends React.Component {
         const tg = this.props.telegram;
         return (
             <article className="app">
+                <div>{this.isExpanded}</div>
                 <AppContainer contentType={Video} empty />
                 <AppContainer
                     contentType={Dialog}
