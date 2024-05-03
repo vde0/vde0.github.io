d7f05b4e08ba4bd20dff4dca0892c6a3544e21d0
P-PRINTER <psevdoprinter@gmail.com>
2024-05-04 00:59:10 +0200

    rm hiding dialog handler for closekeyboard event

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ export default class App extends React.Component {
 
     componentDidMount () {
         window.addEventListener("openkeyboard", evt => this.hideFooter());
-        window.addEventListener("closekeyboard", evt => {this.showFooter(); this.hideDialog()});
+        window.addEventListener("closekeyboard", evt => {this.showFooter();});
     }
 
     render () {
