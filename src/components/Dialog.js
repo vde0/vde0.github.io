00e800340831d806f3921770e9cc06d64a0b4210
P-PRINTER <psevdoprinter@gmail.com>
2024-05-04 12:53:29 +0200

    make HTML autofocus for the msg field

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -162,6 +162,7 @@ export default class Dialog extends React.Component {
                     <input ref={el => this.msgFieldBlock = el}
                         type="text"
                         className="msg-form__field"
+                        autoFocus
                         onInput={this.onInput}/>
                     <Btn
                         type="submit"
