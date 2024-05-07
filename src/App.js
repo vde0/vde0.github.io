28048b50cbfc03de270c58a31b1bafc2b7de23df
P-PRINTER <psevdoprinter@gmail.com>
2024-05-07 13:33:18 +0200

    show logs

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default class App extends React.Component {
     constructor (props) {
         super(props);
 
-        this.log            = false;
+        this.log            = true;
         this.showUpdateNum  = true;
 
         this.onRootClick        = this.onRootClick.bind(this);
