6a123a8e796acb3dc55d780c2d4a916871479c4c
P-PRINTER <psevdoprinter@gmail.com>
2024-05-08 16:01:30 +0200

    fix app containers widthes

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ export default class App extends React.Component {
     render () {
         return (
             <article className="app">
-                {this.showUpdateNum ? <p className="update-num-log">Update num: 23.12</p> : ""}
+                {this.showUpdateNum ? <p className="update-num-log">Update num: 23.13</p> : ""}
                 <div className={"content-log " + (!this.log ? "content-log_hidden" : "")}>
                     <p>Mobile: {String(isMobile)} | iOS: {String(isIOS)}</p>
                     <p>keyboardWasOpened: {String(this.state.keyboardWasOpened)}</p>
