c96dacf8e21069e4b6846e354be313744f721379
P-PRINTER <psevdoprinter@gmail.com>
2024-05-09 22:24:40 +0200

    some correcting for elems sizes settings at the macrotask queue

--- src/components/BottomMenu.js
+++ src/components/BottomMenu.js
@@ -69,7 +69,7 @@ export default class BottomMenu extends React.Component {
                 this.btns.forEach( btn => {
                     this.state[btn.mod + "Handler"] = btn.onClick;
                 });
-            }, 2);
+            }, 3);
         }, {once: true});
     }
 
