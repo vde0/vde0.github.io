f0c9ce1084e5e1ffd1acba542c14a127330c9fb2
P-PRINTER <psevdoprinter@gmail.com>
2024-05-13 17:02:26 +0200

    improve ComponentUpdateHook util

--- src/utils/ComponentUpdateHook.js
+++ src/utils/ComponentUpdateHook.js
@@ -1,18 +1,10 @@
+import TaskManager from "./TaskManager";
+
 export default class ComponentUpdateHook {
     connect (updateFunc, context) {
         this._customUpdate      = updateFunc.bind(context);
-        this._componentUpdate   = context.componentDidUpdate?.bind(context);
-
-        context.componentDidUpdate  = this._update.bind(this);
     }
     on () {
-        this._on = true;
-    }
-    _update () {
-        this._componentUpdate?.();
-
-        if (!this._on) return;
-        this._customUpdate();
-        this._on = false;
+        TaskManager.setMacrotask( _ => this._customUpdate() );
     }
 }
