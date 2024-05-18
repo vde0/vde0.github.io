13ef5b06dbcd79452ac024b2b06557b1f7b09520
P-PRINTER <psevdoprinter@gmail.com>
2024-05-18 12:12:44 +0200

    increase delay from 500ms to 3s 38.3

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -92,7 +92,7 @@ export default class App extends React.Component {
     render () {
         return (
             <article className="app">
-                {this.showUpdateNum ? <p className="update-num-log">Update num: 38.2</p> : ""}
+                {this.showUpdateNum ? <p className="update-num-log">Update num: 38.3</p> : ""}
                 <div className={"content-log " + (!this.log ? "content-log_hidden" : "")}>
                     <p>Mobile: {String(appParams.isMobile)} | iOS: {String(appParams.isIOS)}</p>
                     <p>keyboard state: {String(this.state.keyboardState)}</p>
@@ -141,7 +141,7 @@ export default class App extends React.Component {
 
     hideDialog () {
         this.dialogBlur();
-        setTimeout(_ => this.dialogHook.on(), 500);
+        setTimeout(_ => this.dialogHook.on(), 3000);
         this.setState({dialogShown: false});
     }
     showDialog () {
