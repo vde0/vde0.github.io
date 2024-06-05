031efab0224ce84e55ec1dced1d6901f1cf55dce
P-PRINTER <psevdoprinter@gmail.com>
2024-06-05 17:39:27 +0200

    update execWhenResizeEnd() tg-util and implement it into the keyboard events generation 55.0

--- src/utils/tgUtils.js
+++ src/utils/tgUtils.js
@@ -4,55 +4,70 @@ import { isMobile } from "./utils";
 const telegram  = window.Telegram.WebApp;
 
 
+let isResizing = false;
+let layerCount = 0;
+telegram.onEvent("viewportChanged", _ => {
+    isResizing = true;
+    layerCount++;
+
+    TaskManager.setMacrotask(_ => {
+        if (--layerCount === 0) isResizing = false});
+});
+
 function execWhenResizeEnd (func) {
+
+    const timerId = setInterval(_ => {
+        if (isResizing === true) { clearInterval(timerId); func(); }
+    });
     
-    const exec = (prevHeight) => {
+    // const exec = (prevHeight) => {
         
-        const curHeight = telegram.viewportHeight;
+    //     const curHeight = telegram.viewportHeight;
 
-        if (prevHeight === curHeight) {
-            const checker = {1: null, 2: null, 3: null, 4: null, 5: null}
+    //     if (prevHeight === curHeight) {
+    //         const checker = {1: null, 2: null, 3: null, 4: null, 5: null}
 
-            TaskManager.setMacrotask(_ => checker["1"] = (prevHeight === curHeight), 3);
-            TaskManager.setMacrotask(_ => checker["2"] = (prevHeight === curHeight), 6);
-            TaskManager.setMacrotask(_ => checker["3"] = (prevHeight === curHeight), 9);
-            TaskManager.setMacrotask(_ => checker["4"] = (prevHeight === curHeight), 12);
-            TaskManager.setMacrotask(_ => checker["5"] = (prevHeight === curHeight), 15);
+    //         TaskManager.setMacrotask(_ => checker["1"] = (prevHeight === curHeight), 3);
+    //         TaskManager.setMacrotask(_ => checker["2"] = (prevHeight === curHeight), 6);
+    //         TaskManager.setMacrotask(_ => checker["3"] = (prevHeight === curHeight), 9);
+    //         TaskManager.setMacrotask(_ => checker["4"] = (prevHeight === curHeight), 12);
+    //         TaskManager.setMacrotask(_ => checker["5"] = (prevHeight === curHeight), 15);
 
-            const timerId   = setInterval(_ => {
+    //         const timerId   = setInterval(_ => {
                 
-                if (
-                    checker["1"] === false ||
-                    checker["2"] === false ||
-                    checker["3"] === false ||
-                    checker["4"] === false ||
-                    checker["5"] === false
-                ) {
-                    exec(curHeight); }
+    //             if (
+    //                 checker["1"] === false ||
+    //                 checker["2"] === false ||
+    //                 checker["3"] === false ||
+    //                 checker["4"] === false ||
+    //                 checker["5"] === false
+    //             ) {
+    //                 exec(curHeight); }
                 
-                if (
-                    checker["1"] === true &&
-                    checker["2"] === true &&
-                    checker["3"] === true &&
-                    checker["4"] === true &&
-                    checker["5"] === true
-                ) {
-                    func(); }
+    //             if (
+    //                 checker["1"] === true &&
+    //                 checker["2"] === true &&
+    //                 checker["3"] === true &&
+    //                 checker["4"] === true &&
+    //                 checker["5"] === true
+    //             ) {
+    //                 func(); }
                 
-                if (
-                    checker["1"] !== null &&
-                    checker["2"] !== null &&
-                    checker["3"] !== null &&
-                    checker["4"] !== null &&
-                    checker["5"] !== null
-                ) {
-                    clearInterval(timerId); }
-            });
-        }
-        else    TaskManager.setMacrotask(_ => exec(curHeight), 3);
-    };
+    //             if (
+    //                 checker["1"] !== null &&
+    //                 checker["2"] !== null &&
+    //                 checker["3"] !== null &&
+    //                 checker["4"] !== null &&
+    //                 checker["5"] !== null
+    //             ) {
+    //                 clearInterval(timerId); }
+    //         });
+    //     }
+    //     else    TaskManager.setMacrotask(_ => exec(curHeight), 3);
+    // };
 
-    TaskManager.setMacrotask(_ => exec(telegram.viewportHeight), 3);
+    // TaskManager.setMacrotask(_ => exec(telegram.viewportHeight), 3);
+
     
     // const curHeight = telegram.viewportHeight;
     // trackResize(curHeight);
@@ -155,14 +170,15 @@ if (isMobile) {
 
         const event = new Event(eventName);
 
-        setTimeout(_ => {
-            updateKeyboardState();
-            window.dispatchEvent(event);
-        }, 50);
-        // execWhenResizeEnd(_ => {
-        //     window.dispatchEvent(event);
+        // setTimeout(_ => {
         //     updateKeyboardState();
-        // });
+        //     window.dispatchEvent(event);
+        // }, 50);
+        execWhenResizeEnd(_ => {
+            window.dispatchEvent(event);
+            calcCount++;
+            updateKeyboardState();
+        });
     });
     // closekeyboard event define
     telegram.onEvent("viewportChanged", evt => {
@@ -175,14 +191,15 @@ if (isMobile) {
 
         const event = new Event(eventName);
 
-        setTimeout(_ => {
-            updateKeyboardState();
-            window.dispatchEvent(event);
-        }, 50);
-        // execWhenResizeEnd(_ => {
-        //     window.dispatchEvent(event);
+        // setTimeout(_ => {
         //     updateKeyboardState();
-        // });
+        //     window.dispatchEvent(event);
+        // }, 50);
+        execWhenResizeEnd(_ => {
+            window.dispatchEvent(event);
+            calcCount++;
+            updateKeyboardState();
+        });
     });
 }
 
