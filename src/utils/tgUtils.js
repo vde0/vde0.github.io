9cadbddac598f208ccda87e9ffc120fe68e18030
P-PRINTER <psevdoprinter@gmail.com>
2024-06-06 23:05:29 +0200

    delete useless commented code from the tg-utils 55.5.1

--- src/utils/tgUtils.js
+++ src/utils/tgUtils.js
@@ -21,67 +21,6 @@ function execWhenResizeEnd (func) {
     const timerId = setInterval(_ => {
         if (isResizing === true) { clearInterval(timerId); func(); }
     });
-    
-    // const exec = (prevHeight) => {
-        
-    //     const curHeight = telegram.viewportHeight;
-
-    //     if (prevHeight === curHeight) {
-    //         const checker = {1: null, 2: null, 3: null, 4: null, 5: null}
-
-    //         TaskManager.setMacrotask(_ => checker["1"] = (prevHeight === curHeight), 3);
-    //         TaskManager.setMacrotask(_ => checker["2"] = (prevHeight === curHeight), 6);
-    //         TaskManager.setMacrotask(_ => checker["3"] = (prevHeight === curHeight), 9);
-    //         TaskManager.setMacrotask(_ => checker["4"] = (prevHeight === curHeight), 12);
-    //         TaskManager.setMacrotask(_ => checker["5"] = (prevHeight === curHeight), 15);
-
-    //         const timerId   = setInterval(_ => {
-                
-    //             if (
-    //                 checker["1"] === false ||
-    //                 checker["2"] === false ||
-    //                 checker["3"] === false ||
-    //                 checker["4"] === false ||
-    //                 checker["5"] === false
-    //             ) {
-    //                 exec(curHeight); }
-                
-    //             if (
-    //                 checker["1"] === true &&
-    //                 checker["2"] === true &&
-    //                 checker["3"] === true &&
-    //                 checker["4"] === true &&
-    //                 checker["5"] === true
-    //             ) {
-    //                 func(); }
-                
-    //             if (
-    //                 checker["1"] !== null &&
-    //                 checker["2"] !== null &&
-    //                 checker["3"] !== null &&
-    //                 checker["4"] !== null &&
-    //                 checker["5"] !== null
-    //             ) {
-    //                 clearInterval(timerId); }
-    //         });
-    //     }
-    //     else    TaskManager.setMacrotask(_ => exec(curHeight), 3);
-    // };
-
-    // TaskManager.setMacrotask(_ => exec(telegram.viewportHeight), 3);
-
-    
-    // const curHeight = telegram.viewportHeight;
-    // trackResize(curHeight);
-
-    // function trackResize (prevHeight) {
-
-    //     TaskManager.setMacrotask(_ => {
-    //         const curHeight = telegram.viewportHeight;
-    //         if (curHeight === prevHeight) func();
-    //         else    trackResize(curHeight);
-    //     }, 10);
-    // }
 }
 
 let maxHeight     = null;
@@ -90,14 +29,7 @@ window.addEventListener("load", _ => {
     telegram.onEvent("viewportChanged", _ => {
         if (telegram.viewportHeight > maxHeight) maxHeight = telegram.viewportHeight;
     });
-    // execWhenResizeEnd(_ => {maxHeight = telegram.viewportHeight; maxHeightCalcTime++});
-    // telegram.onEvent("viewportChanged", evt => {
-    //     if (telegram.viewportHeight > maxHeight) maxHeight = telegram.viewportHeight;
-    // });
 }, {once: true});
-// function resetmaxHeight () {
-//     maxHeight     = telegram.viewportStableHeight;
-// }
 
 function checkMobileKeyboard () {
     if (!isMobile) return false;
@@ -108,39 +40,6 @@ function checkMobileKeyboard () {
 
 let calcCount = 0;
 if (isMobile) {
-
-    // let isCalc  = false;
-    // // openkeyboard event define
-    // telegram.onEvent("viewportChanged", evt => {
-        
-    //     if (isCalc) return;
-    //     isCalc = true;
-    //     calcCount++;
-
-    //     const startHeight   = telegram.viewportHeight;
-    //     // const isOpened  = checkMobileKeyboard();
-    //     const openName  = "openkeyboard";
-    //     const closeName = "closekeyboard";
-
-    //     // if (prevKeyboardState === isOpened) return;
-
-    //     // if (!isOpened)                      return;
-
-    //     execWhenResizeEnd(_ => {
-    //         const curHeight = telegram.viewportHeight;
-    //         isCalc = false;
-    //         // if (curHeight === startHeight) throw new ErrorEvent(`the start height (${startHeight}) and the current height (${curHeight}) are equal in the result of the generation closekeyboard and openkeyboard events.`)
-    //         if (curHeight === startHeight) return;
-    //         const eventName = curHeight > startHeight
-    //             ? closeName
-    //             : openName;
-            
-    //         const event     = new Event(eventName, {bubbles: true});
-    //         window.dispatchEvent(event);
-    //         // updateKeyboardState();
-    //     });
-    // });
-
 
     let prevKeyboardState = checkMobileKeyboard();
     let isCalc = false;
@@ -164,10 +63,6 @@ if (isMobile) {
 
         const event = new Event(eventName);
 
-        // setTimeout(_ => {
-        //     updateKeyboardState();
-        //     window.dispatchEvent(event);
-        // }, 50);
         execWhenResizeEnd(_ => {
             window.dispatchEvent(event);
             updateKeyboardState();
@@ -175,27 +70,6 @@ if (isMobile) {
             isCalc = false;
         });
     }), {once: true});
-    // closekeyboard event define
-    // telegram.onEvent("viewportChanged", evt => {
-    //     const isOpened  = checkMobileKeyboard();
-    //     const eventName = "closekeyboard";
-
-    //     if (prevKeyboardState === isOpened) return;
-    //     updateKeyboardState();
-
-    //     if (isOpened)                       return;
-
-    //     const event = new Event(eventName);
-
-    //     // setTimeout(_ => {
-    //     //     updateKeyboardState();
-    //     //     window.dispatchEvent(event);
-    //     // }, 50);
-    //     execWhenResizeEnd(_ => {
-    //         window.dispatchEvent(event);
-    //         calcCount++;
-    //     });
-    // });
 }
 
 export {
