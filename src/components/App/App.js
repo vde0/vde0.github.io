3f31c38a6f2b158ee1885b6e430bf0243044b016
P-PRINTER <psevdoprinter@gmail.com>
2024-06-16 15:25:39 +0200

    add borders form the dialog-block 65.6

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -171,7 +171,7 @@ export default class App extends React.Component {
     render () {
         return (
             <article className="app">
-                {this.showUpdateNum ? <p className="update-num-log">Update num: 65.5.3</p> : ""}
+                {this.showUpdateNum ? <p className="update-num-log">Update num: 65.6</p> : ""}
                 <div className={"content-log " + (!this.log ? "content-log_hidden" : "")}>
                     <p>Mobile: {String(isMobile)} | iOS: {String(isIOs)}</p>
                     <p>keyboard open state: {String(this.state.keyboardState)}</p>
