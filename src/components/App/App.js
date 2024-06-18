9692a8e9b961bcca9078ae78c2951e1c30ea5d00
P-PRINTER <psevdoprinter@gmail.com>
2024-06-18 14:01:48 +0200

    update cancel btn sizes 69.2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -126,7 +126,7 @@ export default class App extends React.Component {
     render () {
         return <>
             <article className="app">
-                {this.showUpdateNum ? <p className="update-num-log">Update num: 69.1</p> : ""}
+                {this.showUpdateNum ? <p className="update-num-log">Update num: 69.2</p> : ""}
                 <div className={"content-log " + (!this.log ? "content-log_hidden" : "")}>
                     <p>Mobile: {String(isMobile)} | iOS: {String(isIOs)}</p>
                     <p>keyboard open state: {String(this.state.keyboardState)}</p>
