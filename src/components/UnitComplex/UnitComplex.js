5ac10be8209bf212d6641493c5881c26144f2ebd
P-PRINTER <psevdoprinter@gmail.com>
2024-06-21 22:24:26 +0200

    some commenting

--- src/components/UnitComplex/UnitComplex.js
+++ src/components/UnitComplex/UnitComplex.js
@@ -6,22 +6,27 @@ export default class UnitComplex extends React.Component {
 
     render () {
         return <>
+            {/* dvh */}
             <Unit mode="d" verticalAlignment="top" horizontalAlignment="left" size="large" color="blue" />
             <Unit mode="d" verticalAlignment="middle" horizontalAlignment="left" size="large" color="blue" />
             <Unit mode="d" verticalAlignment="bottom" horizontalAlignment="left" size="large" color="blue" />
 
+            {/* lvh */}
             <Unit mode="l" verticalAlignment="top" horizontalAlignment="middle" size="large" color="green" />
             <Unit mode="l" verticalAlignment="middle" horizontalAlignment="middle" size="large" color="green" />
             <Unit mode="l" verticalAlignment="bottom" horizontalAlignment="middle" size="large" color="green" />
 
+            {/* svh */}
             <Unit mode="s" verticalAlignment="top" horizontalAlignment="right" size="large" color="red" />
             <Unit mode="s" verticalAlignment="middle" horizontalAlignment="right" size="large" color="red" />
             <Unit mode="s" verticalAlignment="bottom" horizontalAlignment="right" size="large" color="red" />
 
+            {/* vh */}
             <Unit mode="n" verticalAlignment="top" horizontalAlignment="left" size="small" color="orange" />
             <Unit mode="n" verticalAlignment="middle" horizontalAlignment="left" size="small" color="orange" />
             <Unit mode="n" verticalAlignment="bottom" horizontalAlignment="left" size="small" color="orange" />
 
+            {/* % */}
             <Unit verticalAlignment="top" horizontalAlignment="right" size="small" color="purple" />
             <Unit verticalAlignment="middle" horizontalAlignment="right" size="small" color="purple" />
             <Unit verticalAlignment="bottom" horizontalAlignment="right" size="small" color="purple" />
