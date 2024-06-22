8f905ff63ce3538cf0e8ae5312798547ec052059
P-PRINTER <psevdoprinter@gmail.com>
2024-06-22 12:51:10 +0200

    make doc scrollable 70.0

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -126,7 +126,7 @@ export default class App extends React.Component {
     render () {
         return <>
             <article className="app">
-                {this.showUpdateNum ? <p className="update-num-log">Update num: 69.5</p> : ""}
+                {this.showUpdateNum ? <p className="update-num-log">Update num: 70</p> : ""}
                 <div className={"content-log " + (!this.log ? "content-log_hidden" : "")}>
                     <p>Mobile: {String(isMobile)} | iOS: {String(isIOs)}</p>
                     <p>keyboard open state: {String(this.state.keyboardState)}</p>
