eb4cb272028c2335a94ec3c274aa82afdf6307bc
P-PRINTER <psevdoprinter@gmail.com>
2024-07-04 19:12:56 +0200

    make msg-form pos like bottom=0 71.4.1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -140,7 +140,7 @@ export default class App extends React.Component {
     render () {
         return <>
             <article className="app">
-                {this.showUpdateNum ? <p className="update-num-log">Update num: 71.4</p> : ""}
+                {this.showUpdateNum ? <p className="update-num-log">Update num: 71.4.1</p> : ""}
                 <div className={"content-log " + (!this.log ? "content-log_hidden" : "")}>
                     <p>Mobile: {String(isMobile)} | iOS: {String(isIOs)}</p>
                     {/* <p>keyboard open state: {String(this.state.keyboardState)}</p>
