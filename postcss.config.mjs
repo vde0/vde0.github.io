121e2f3d2c455e417914c03946dcac3d63651ac8
P-PRINTER <psevdoprinter@gmail.com>
2025-03-07 20:16:41 +0100

    rm purgecss and @tailwindcss/cli packs config tailwindcss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,6 @@
 export default {
     plugins: {
       "@tailwindcss/postcss": {}, // Подключаем Tailwind CSS
-      "@fullhuman/postcss-purgecss": {
-        content: [
-          './src/**/*.{html,js,jsx,ts,tsx,vue}'],  // Указываем, какие файлы будут сканироваться на наличие классов
-        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],  // Извлечение классов
-      },
       autoprefixer: {},  // Автопрефиксация для совместимости с браузерами
       cssnano: { preset: "default" },  // Минификация CSS
     },
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+      "./src/**/*.{html,js,jsx,ts,tsx}",
+      "./public/*.html",
+    ],
+    theme: {
+      extend: {},
+    },
+    corePlugins: {
+      padding: true,
+    },
+    plugins: [],
+  }
+  
