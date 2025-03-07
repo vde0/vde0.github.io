5c33f00ac165e93e884a1fbaf162c0983a677734
P-PRINTER <psevdoprinter@gmail.com>
2025-03-07 15:59:54 +0100

    Wrap app content within the Contexts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import React from 'react';
 import Main from './containers/Main';
+import TextChatProvider from '@store/TextChatProvider';
+import ConnectProvider from '@store/ConnectProvider';
 
+
+const MemoMain              = React.memo( Main );
+const MemoTextChatProvider  = React.memo( TextChatProvider );
 
 const App: React.FC = () => (
-    <Main />
+    <ConnectProvider>
+        <MemoTextChatProvider>
+            <MemoMain />
+        </MemoTextChatProvider>
+    </ConnectProvider>
 );
 
 
