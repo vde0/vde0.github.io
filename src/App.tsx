8810989a87d83767aefb9c333a811ba8f89cf9d5
P-PRINTER <psevdoprinter@gmail.com>
2025-03-11 12:00:30 +0100

    Add places for the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import ConnectProvider from '@store/ConnectProvider';
 import { WebAppProvider } from '@vkruglikov/react-telegram-web-app';
 import PlatformProvider from '@store/PlatformProvider';
 
+
 const MemoMain              = React.memo( Main );
 const MemoTextChatProvider  = React.memo( TextChatProvider );
 const MemoConnectProvider   = React.memo( ConnectProvider );
 const MemoPlatformProvider  = React.memo( PlatformProvider );
+
 
 const App: React.FC = () => (
     <WebAppProvider>
