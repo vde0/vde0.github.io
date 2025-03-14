f10042ca982da10d92c64be36fd804c77a6a809b
P-PRINTER <psevdoprinter@gmail.com>
2025-03-14 21:32:30 +0100

    -(rm) the makeSubmit util/HOCl; +(add) a provideFormStatus util/HOC

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -2,24 +2,22 @@ import { RefObject } from "react";
 import { FormStatus, useFormStatus } from "react-dom";
 
 
-type Submitable = {[key: string]: any, type: string};
-type SubmitProps<P extends Submitable> = 
-    & Omit<P, "type">
-    & { formStatus?: RefObject<FormStatus | null> }
-    & React.PropsWithChildren;
-type Submit<P extends Submitable> = React.FC< SubmitProps<P> >;
+type FormStatusRef = RefObject<FormStatus | null>;
+type PropsWithFormStatus = { formStatus?: FormStatusRef };
 
-const makeSubmit = <P extends Submitable>(Comp: React.FC<P>): Submit<P> => {
+type FormStatusFC<P> = React.FC<P & PropsWithFormStatus>
 
-    return ({ children, formStatus, ...props })=> {
+const provideFormStatus = <P = {}>(Comp: React.FC<P>): FormStatusFC<P> => {
+
+    return ( {formStatus, ...props} ) => {
 
         if (formStatus?.current) formStatus.current = useFormStatus();
-        return <Comp {...{type: "submit", ...props} as P}>{children}</Comp>
+        return <Comp {...props as P & PropsWithFormStatus} />;
     };
 };
 
 
 export {
-    makeSubmit,
-    Submitable, SubmitProps, Submit,
+    provideFormStatus,
+    FormStatusRef, PropsWithFormStatus, FormStatusFC,
 };
