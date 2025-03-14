9deba9bbdad94240a8f05f87ad79cf4570e71d50
P-PRINTER <psevdoprinter@gmail.com>
2025-03-14 22:40:02 +0100

    ~ update default vals of generics of the utils/provideFormStatus

--- src/utils/provideFormStatus.tsx
+++ src/utils/provideFormStatus.tsx
@@ -5,9 +5,9 @@ import { FormStatus, useFormStatus } from "react-dom";
 type FormStatusRef = RefObject<FormStatus | null>;
 type PropsWithFormStatus = { formStatus?: FormStatusRef };
 
-type FormStatusFC<P> = React.FC<P & PropsWithFormStatus>
+type FormStatusFC<P extends object = {}> = React.FC<P & PropsWithFormStatus>
 
-const provideFormStatus = <P = {}>(Comp: React.FC<P>): FormStatusFC<P> => {
+const provideFormStatus = <P extends object = {}>(Comp: React.FC<P>): FormStatusFC<P> => {
 
     return ( {formStatus, ...props} ) => {
 
