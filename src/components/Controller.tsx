68a08c700221446857f70daf752ebce52b16964b
P-PRINTER <psevdoprinter@gmail.com>
2025-03-15 14:58:33 +0100

    +(add) handler props for the Controller +(use) the useMobile hook for the Controller

--- src/components/Controller.tsx
+++ src/components/Controller.tsx
@@ -1,18 +1,40 @@
-/** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
-import Btn from "./Btn";
+import Btn, { BtnProps } from "./Btn";
+import { useCheckMobile } from "@hooks";
+import { useMemo } from "react";
 
 
-const Controller: React.FC = () => (
-    <section className="
-        flex items-stretch justify-between gap-3
-        absolute left-3 right-3 md:left-8 md:right-8 bottom-0
-        box-content h-15 py-4
-    ">
-        <Btn className="bg-blue-400" />
-        <Btn className="bg-blue-400" />
-        <Btn className="bg-blue-400"  />
-    </section>
-);
+const MOBILE_HANDLER: keyof BtnProps = "onTouch";
+const DESKTOP_HANDLER: keyof BtnProps = "onClick";
+
+
+type TurnEvent = React.MouseEvent | React.TouchEvent;
+
+interface ContrtollerProps {
+    onTextChat?: (e?: TurnEvent) => void;
+    onAddUser?: (e?: TurnEvent) => void;
+    onNext?: (e?: TurnEvent) => void;
+}
+
+const Controller: React.FC<ContrtollerProps> = ({ onTextChat, onAddUser, onNext }) => {
+    
+    const isMobile  = useCheckMobile();
+    const onTurn    = useMemo<string>(() => isMobile ? MOBILE_HANDLER : DESKTOP_HANDLER, [isMobile]);
+
+    return (
+        <section className="
+            flex items-stretch justify-between gap-3
+            absolute left-3 right-3 md:left-8 md:right-8 bottom-0
+            box-content h-15 py-4
+        ">  
+            // TextChat
+            <Btn className="bg-blue-400" {...{[onTurn]: onTextChat}} />
+            // AddUser
+            <Btn className="bg-blue-400" {...{[onTurn]: onAddUser}} />
+            // Next
+            <Btn className="bg-blue-400" {...{[onTurn]: onNext}} />
+        </section>
+    )
+};
+
 
 export default Controller
