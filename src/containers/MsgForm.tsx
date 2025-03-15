26e302868b56807b6a9e6c0bbfe46f849306d562
P-PRINTER <psevdoprinter@gmail.com>
2025-03-15 15:23:44 +0100

    -(rm) utils/FormStatusGetter

--- src/containers/MsgForm.tsx
+++ src/containers/MsgForm.tsx
@@ -1,13 +1,8 @@
 import { SubmitBtn } from "@components/Btn";
 import InputMsg from "@components/InputMsg";
-import { FormStatusGetter, FormStatusRef } from "@utils";
-import { useRef } from "react";
-import { FormStatus } from "react-dom";
 
 
 const MsgForm: React.FC = () => {
-
-    const formStatusRef = useRef<FormStatus | null>(null);
 
     return (
         <form className="
@@ -16,13 +11,10 @@ const MsgForm: React.FC = () => {
             box-content pb-1 pt-2 bottom-0
             flex flex-row gap-1"
         action="">
-            <InputMsg disabled={formStatusRef?.current?.pending} />
+            <InputMsg />
             <SubmitBtn
-                disabled={formStatusRef?.current?.pending}
                 className="bg-black border-1 border-gray-700"
             />
-
-            <FormStatusGetter ref={formStatusRef} />
         </form>
     );
 };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,1 @@
 export * from "./bindProps";
-export * from "./FormStatusGetter";
