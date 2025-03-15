0c146f25d24b62395e5c65cec3f90d92907df950
P-PRINTER <psevdoprinter@gmail.com>
2025-03-15 15:02:23 +0100

    +(add) InputMsg and SubmitBtn into the MsgForm +(use) FormStatusGetter for the MsgForm

--- src/containers/MsgForm.tsx
+++ src/containers/MsgForm.tsx
@@ -1,7 +1,14 @@
-import Btn from "@components/Btn";
+import { SubmitBtn } from "@components/Btn";
+import InputMsg from "@components/InputMsg";
+import { FormStatusGetter, FormStatusRef } from "@utils";
+import { useRef } from "react";
+import { FormStatus } from "react-dom";
 
 
 const MsgForm: React.FC = () => {
+
+    const formStatusRef = useRef<FormStatus | null>(null);
+
     return (
         <form className="
             fixed z-1 bg-green-400
@@ -9,8 +16,13 @@ const MsgForm: React.FC = () => {
             box-content pb-1 pt-2 bottom-0
             flex flex-row gap-1"
         action="">
-            <textarea className="grow bg-[#2C2C2C] text-white p-2 text-xl" name="" id="" />
-            <Btn />
+            <InputMsg disabled={formStatusRef?.current?.pending} />
+            <SubmitBtn
+                disabled={formStatusRef?.current?.pending}
+                className="bg-black border-1 border-gray-700"
+            />
+
+            <FormStatusGetter ref={formStatusRef} />
         </form>
     );
 };
