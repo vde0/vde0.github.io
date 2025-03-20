948e481227957398b87a93c19ae030d3333e5ef9
P-PRINTER <psevdoprinter@gmail.com>
2025-03-21 00:57:41 +0100

    ~(update & fix (maybe add)) mockTelegram() using deepCopy() from prev commit for the test-utils/

--- src/test-utils/index.ts
+++ src/test-utils/index.ts
@@ -1,44 +1,51 @@
-const mockTelegram = (): void => {
+import { deepCopy } from "@utils";
+import { TWebApp } from "@vkruglikov/react-telegram-web-app/lib/core/twa-types";
 
-    global.window.Telegram = {
-        WebApp: {
-        platform: "android",
-        initData: "",
-        initDataUnsafe: {
-            auth_date: 0,
-            hash: "",
-        },
-        colorScheme: "dark",
-        themeParams: {},
-        isExpanded: false,
-        viewportHeight: 0,
-        viewportStableHeight: 0,
-        MainButton: {
-            text: "",
-            color: "",
-            textColor: "",
-            isActive: false,
-            isVisible: false,
-            isProgressVisible: false,
-            setText () { return this; },
-            show () { return this; },
-            hide () { return this; },
-            enable () { return this; },
-            disable () { return this; },
-            showProgress () { return this; },
-            hideProgress () { return this; },
-            onClick () { return this; },
-            offClick () { return this; },
-            setParams () { return this },
-        },
-        sendData () {},
-        ready () {},
-        expand () {},
-        close () {},
-        onEvent () {},
-        offEvent () {},
-        },
-    };
+
+const DEFAULT_WEBAPP = {
+    platform: "android",
+    initData: "",
+    initDataUnsafe: {
+        auth_date: 0,
+        hash: "",
+    },
+    colorScheme: "dark",
+    themeParams: {},
+    isExpanded: false,
+    viewportHeight: 0,
+    viewportStableHeight: 0,
+    MainButton: {
+        text: "",
+        color: "",
+        textColor: "",
+        isActive: false,
+        isVisible: false,
+        isProgressVisible: false,
+        setText () { return this; },
+        show () { return this; },
+        hide () { return this; },
+        enable () { return this; },
+        disable () { return this; },
+        showProgress () { return this; },
+        hideProgress () { return this; },
+        onClick () { return this; },
+        offClick () { return this; },
+        setParams () { return this },
+    },
+    sendData () {},
+    ready () {},
+    expand () {},
+    close () {},
+    onEvent () {},
+    offEvent () {},
 };
 
-export { mockTelegram };
+
+const mockTelegram = (): void => {
+    if (!window.Telegram) window.Telegram = { WebApp: {} as TWebApp };
+    global.window.Telegram.WebApp = deepCopy(DEFAULT_WEBAPP, {depth: -1}) as TWebApp;
+};
+
+export {
+    mockTelegram,
+};
