d14c89655e027ade52d5e596ce542a3017799d86
P-PRINTER <psevdoprinter@gmail.com>
2025-04-15 00:08:57 +0200

    + Implement upgrade of general Connect structure for the VideoChat container

--- src/containers/VideoChat.tsx
+++ src/containers/VideoChat.tsx
@@ -1,9 +1,22 @@
+import { useRemoteMedia } from "@hooks";
 import { PropsWithClassName } from "@types";
+import { useEffect, useRef } from "react";
+
 
 const VideoChat: React.FC<PropsWithClassName> = ({ className }) => {
     
+    const ref           = useRef<HTMLVideoElement | null>(null)
+    const remoteMedia   = useRemoteMedia();
+
+    useEffect(() => {
+        if (!ref.current) return;
+        ref.current.srcObject = remoteMedia;
+    }, [remoteMedia]);
+
     return (
-        <section className={className}></section>
+        <section className={className}>
+            <video ref={ref} autoPlay />
+        </section>
     );
 };
 
