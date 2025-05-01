e7e698c01d05993e3aee47ac916e82050d5acac8
P-PRINTER <psevdoprinter@gmail.com>
2025-05-02 00:27:30 +0200

    ~ Fix issue with remote peer media not playing due to "user gesture" security policy !But muted=true should be deleted

--- src/containers/VideoChat.tsx
+++ src/containers/VideoChat.tsx
@@ -1,4 +1,5 @@
-import { useLocalMedia, useRemoteMedia } from "@hooks";
+import { useLocalVideo, useRemoteMedia } from "@hooks";
+import { addDebug } from "@lib/utils";
 import { PropsWithClassName } from "@types";
 import { useEffect, useRef } from "react";
 
@@ -9,16 +10,20 @@ const VideoChat: React.FC<VideoChatProps> = ({ className, remote = false }) => {
     
     const ref           = useRef<HTMLVideoElement | null>(null)
     const remoteMedia   = useRemoteMedia();
-    const localMedia    = useLocalMedia();
+    const localMedia    = useLocalVideo();
 
     useEffect(() => {
         if (!ref.current) return;
         ref.current.srcObject = remote ? remoteMedia : localMedia;
+        ref.current.getAttribute("id") && addDebug(ref.current.getAttribute("id") as string, ref.current);
+
+        // TODO: IMPLEMENT PLAYING BY USER GESTURE
+        ref.current.play();
     }, [localMedia, remoteMedia, remote]);
 
     return (
         <section className={className}>
-            <video id={remote?"remoteVideo":"localVideo"} muted={!remote} ref={ref} autoPlay />
+            <video id={remote?"remoteVideo":"localVideo"} ref={ref} playsInline muted={true} />
         </section>
     );
 };
