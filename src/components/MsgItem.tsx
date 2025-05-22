dab160f63418ec55ff83155f68293c3df2afbff4
P-PRINTER <psevdoprinter@gmail.com>
2025-05-22 22:28:52 +0200

    ~Make msgs like dialogue clouds

--- src/components/MsgItem.tsx
+++ src/components/MsgItem.tsx
@@ -6,12 +6,20 @@ interface MsgItemProps {
 
 const MsgItem: React.FC<MsgItemProps> = ({ sender, text, direction }) => (
     <section className={`mb-2 p-2 rounded-2xl w-2/3 relative
-        ${direction==="left"?"bg-gray left-2":"bg-light-blue right-2 ml-auto"}`
+        ${direction==="left"?"bg-gray rounded-bl-none left-2":"bg-light-blue rounded-br-none right-2 ml-auto"}`
     }>
         <span className={`block font-semibold ${direction==="left"?"text-light-blue":"text-orange"}`}>
             {sender}
         </span>
         <p className="text-white">{text}</p>
+        <div className={`
+            absolute w-0 h-0 bottom-0
+            border-t-transparent border-4
+            ${direction==="left"
+                ?"border-gray border-l-transparent right-full"
+                :"border-light-blue border-r-transparent left-full"
+            }
+        `}></div>
     </section>
 );
 
