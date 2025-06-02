49c8efb463c6325b2d22b16e65be552dc325c96e
P-PRINTER <psevdoprinter@gmail.com>
2025-06-02 03:51:46 +0200

    +Code types are PascalCase, CamelCase, LowerSnakeCase,  UpperSnakeCase

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,3 +1,62 @@
 export type PropsWithClassName = { className?: string };
 export type BasicDataStruct = { [key: string | symbol | number]: any };
 export type ContentMedia = MediaStream | null;
+
+export type IsUncapitale<L extends string> = L extends Uncapitalize<L> ? true : false;
+export type IsCapitale<L extends string> = L extends Capitalize<L> ? true : false;
+export type IsUpper<L extends string> = L extends Uppercase<L> ? true : false;
+export type IsLower<L extends string> = L extends Lowercase<L> ? true : false;
+export type IsEmpty<L extends string> = L extends '' ? true : false;
+
+export type IfUncapitale<L extends string, Then, Else> = IsUncapitale<L> extends true ? Then : Else;
+export type IfCapitale<L extends string, Then, Else> = IsCapitale<L> extends true ? Then : Else;
+export type IfUpper<L extends string, Then, Else> = IsUpper<L> extends true ? Then : Else;
+export type IfLower<L extends string, Then, Else> = IsLower<L> extends true ? Then : Else;
+export type IfEmpty<L extends string, Then, Else> = IsEmpty<L> extends true ? Then : Else;
+
+export type WithoutSplitters<
+	L extends string,
+	C extends string = '-' | '_'
+> = L extends `${infer H}${infer T}`
+	? H extends C
+		? WithoutSplitters<Capitalize<T>>
+		: `${H}${WithoutSplitters<T>}`
+	: '';
+
+export type CamelCase<S extends string> = WithoutSplitters<LowerSnakeCase<S>>;
+export type PascalCase<S extends string> = Capitalize<CamelCase>;
+
+export type LowerSnakeCase<S extends string> =
+	WithoutSplitters<S> extends `${infer H}${infer B}${infer T}`
+		? IfUncapitale<
+				H,
+				IfUncapitale<
+					B,
+					IfUncapitale<
+						T,
+						`${H}${B}${LowerSnakeCase<T>}`, // hbt...
+						`${H}${B}_${LowerSnakeCase<T>}` // hbT...
+					>,
+					`${H}_${LowerSnakeCase<`${B}${T}`>}` // hBt | hBT...
+				>,
+				IfUncapitale<
+					B,
+					IfUncapitale<
+						T,
+						`${Lowercase<H>}${B}${LowerSnakeCase<T>}`, // Hbt...
+						`${Lowercase<H>}${B}_${LowerSnakeCase<T>}` // HbT...
+					>,
+					IfUncapitale<
+						T,
+						IfEmpty<
+							T,
+							`${Lowercase<H>}${Lowercase<B>}`, // HB
+							`${Lowercase<H>}_${LowerSnakeCase<`${B}${T}`>}` // HBt... !!!
+						>,
+						`${Lowercase<H>}${LowerSnakeCase<`${B}${T}`>}` // HBT...
+					>
+				>
+		  >
+		: Lowercase<S>;
+
+export type UpperSnakeCase<S extends string> = Uppercase<LowerSnakeCase<S>>;
