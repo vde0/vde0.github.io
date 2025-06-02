75b5fc669875cb6bb7651bf2b5bb41eabf69859a
P-PRINTER <psevdoprinter@gmail.com>
2025-06-02 18:02:22 +0200

    -Delete laztInit for localMedia

--- src/api/localMedia.ts
+++ src/api/localMedia.ts
@@ -1,5 +1,4 @@
 import { ContentMedia } from '@types';
-import { once } from '@utils';
 
 // === DATA ===
 export type LocalMediaHandler = (localMedia: ContentMedia) => void;
@@ -12,32 +11,6 @@ const execHandlers = (stream: ContentMedia): void => {
 	for (let h of handlers) h(stream);
 	handlers.clear();
 };
-
-const lazyInit = once((): void => {
-	navigator.mediaDevices
-		.getUserMedia({
-			video: {
-				frameRate: {
-					ideal: 10,
-					max: 15,
-				},
-				width: { ideal: 1280 },
-				height: { ideal: 720 },
-				aspectRatio: { ideal: 16 / 9 },
-				facingMode: 'user',
-			},
-			audio: true,
-		})
-		.then((stream) => {
-			localMedia = stream;
-			execHandlers(localMedia);
-		})
-		.catch((err) => {
-			localMedia = null;
-			console.error('Error getting local media stream:', err);
-			execHandlers(localMedia);
-		});
-});
 
 // === PUBLIC API ===
 export const whenLocalMedia = (handler: LocalMediaHandler): void => {
