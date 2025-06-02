02c8a7fafcb58aafc42d6e58b841dbe65dd94812
P-PRINTER <psevdoprinter@gmail.com>
2025-06-02 18:02:10 +0200

    ~Fix Signal for prev EventKeys update

--- src/api/Signal.ts
+++ src/api/Signal.ts
@@ -1,98 +1,93 @@
-import { io, Socket } from "socket.io-client";
-import { EventKeys, IListenerChest, ListenerChest } from "@lib/pprinter-tools";
-import { addDebug, listen } from "@lib/utils";
-
+import { io, Socket } from 'socket.io-client';
+import { EventKeys, IListenerChest, ListenerChest } from '@lib/pprinter-tools';
+import { addDebug, listen } from '@lib/utils';
 
 // === GENERAL DATA ===
-addDebug("signalHost", process.env.SIGNAL);
+addDebug('signalHost', process.env.SIGNAL);
 export const SIGNAL_SERVER = process.env.SIGNAL ?? 'https://vde0.chat';
-
 
 // === TYPES ===
 export type SignalConstructor = new () => Signal;
 export type Signal = IListenerChest<SignalEventMap> & {
-    start       ():                                                         void;
-    stop        ():                                                         void;
+	start(): void;
+	stop(): void;
 
-    relayIce    (ice: RTCIceCandidate | null):                              void;
-    relaySdp    (sdp: RTCSessionDescription | RTCSessionDescriptionInit):   void;
+	relayIce(ice: RTCIceCandidate | null): void;
+	relaySdp(sdp: RTCSessionDescription | RTCSessionDescriptionInit): void;
 };
 
-
 export type SignalEventMap = {
-    'start':        undefined;
-    'stop':         undefined;
-    'setconfig':    undefined;
+	start: undefined;
+	stop: undefined;
+	setConfig: undefined;
 
-    'offer':    undefined,
-    'sdp':      {sdp: RTCSessionDescription | RTCSessionDescriptionInit},
-    'ice':      {ice: RTCIceCandidate | null},
+	offer: undefined;
+	sdp: { sdp: RTCSessionDescription | RTCSessionDescriptionInit };
+	ice: { ice: RTCIceCandidate | null };
 };
 export type SignalEvent = keyof SignalEventMap;
 export const SIGNAL_EVENTS: EventKeys<SignalEvent> = {
-    START:         "start",
-    STOP:           "stop",
-    SETCONFIG:      "setconfig",
+	START: 'start',
+	STOP: 'stop',
+	SET_CONFIG: 'setconfig',
 
-    OFFER:  "offer",
-    SDP:    "sdp",
-    ICE:    "ice",
+	OFFER: 'offer',
+	SDP: 'sdp',
+	ICE: 'ice',
 } as const;
 Object.freeze(SIGNAL_EVENTS);
 
-
 // === FABRIC OF CONNECTION ===
 export const Signal: SignalConstructor = function (): Signal {
+	// === PRIVATE FIELDS AND METHODS ===
+	let socket: Socket;
+	let target: string;
 
-    // === PRIVATE FIELDS AND METHODS ===
-    let socket: Socket;
-    let target: string;
+	let state: 'new' | 'started' | 'stopped' = 'new';
 
-    let state:      'new' | 'started' | 'stopped'   = "new";
+	const chest: IListenerChest<SignalEventMap> = new ListenerChest();
 
-    const chest:    IListenerChest<SignalEventMap>  = new ListenerChest();
+	const signal = (): void => {
+		socket = io(SIGNAL_SERVER);
 
-    const signal    = (): void => {
-        socket = io(SIGNAL_SERVER);
+		listen(socket, {
+			accepttarget: ({ target: id, offer }: { target: string; offer: boolean }) => {
+				target = id;
+				offer && chest.exec(SIGNAL_EVENTS.OFFER);
+			},
+			acceptsdp: ({ sdp }: { sdp: RTCSessionDescription | RTCSessionDescriptionInit }) => {
+				chest.exec(SIGNAL_EVENTS.SDP, { sdp });
+			},
+			acceptice: ({ ice }: { ice: RTCIceCandidate | null }) => {
+				chest.exec(SIGNAL_EVENTS.ICE, { ice });
+			},
+		});
+	};
 
-        listen(socket, {
-            accepttarget: ({ target: id, offer }: {target: string, offer: boolean}) => {
-                target = id;
-                offer && chest.exec(SIGNAL_EVENTS.OFFER);
-            },
-            acceptsdp: ({ sdp }: {sdp: RTCSessionDescription | RTCSessionDescriptionInit}) => {
-                chest.exec(SIGNAL_EVENTS.SDP, {sdp});
-            },
-            acceptice: ({ ice }: {ice: RTCIceCandidate | null}) => {
-                chest.exec(SIGNAL_EVENTS.ICE, {ice});
-            },
-        });
-    };
+	chest.once('start', () => {
+		signal();
+	});
 
+	// === RESULT INSTANCE ===
+	return {
+		...chest,
+		start() {
+			if (state !== 'new') return;
+			state = 'started';
+			chest.exec(SIGNAL_EVENTS.START);
+		},
+		stop() {
+			if (state !== 'started') return;
+			state = 'stopped';
+			socket.emit('success');
+			chest.exec(SIGNAL_EVENTS.STOP);
+		},
 
-    chest.once("start", () => { signal(); });
-
-
-    // === RESULT INSTANCE ===
-    return {
-        ...chest,
-        start ()    {
-            if (state !== "new") return;
-            state = "started";
-            chest.exec(SIGNAL_EVENTS.START);
-        },
-        stop ()      {
-            if (state !== "started") return;
-            state = "stopped";
-            socket.emit("success");
-            chest.exec(SIGNAL_EVENTS.STOP);
-        },
-
-        relaySdp (sdp) {
-            socket.emit("relaysdp", { target, sdp });
-        },
-        relayIce (ice) {
-            socket.emit("relayice", { target, ice })
-        },
-    };
+		relaySdp(sdp) {
+			socket.emit('relaysdp', { target, sdp });
+		},
+		relayIce(ice) {
+			socket.emit('relayice', { target, ice });
+		},
+	};
 } as unknown as SignalConstructor;
