790493702a952d9084007e7567956041a0328a7c
P-PRINTER <psevdoprinter@gmail.com>
2025-06-02 12:00:22 +0200

    ~Fix prev type updates for English semantic

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -2,14 +2,18 @@ export type PropsWithClassName = { className?: string };
 export type BasicDataStruct = { [key: string | symbol | number]: any };
 export type ContentMedia = MediaStream | null;
 
-export type IsUncapitale<L extends string> = L extends Uncapitalize<L> ? true : false;
-export type IsCapitale<L extends string> = L extends Capitalize<L> ? true : false;
+export type IsUncapitalized<L extends string> = L extends Uncapitalize<L> ? true : false;
+export type IsCapitalized<L extends string> = L extends Capitalize<L> ? true : false;
 export type IsUpper<L extends string> = L extends Uppercase<L> ? true : false;
 export type IsLower<L extends string> = L extends Lowercase<L> ? true : false;
 export type IsEmpty<L extends string> = L extends '' ? true : false;
 
-export type IfUncapitale<L extends string, Then, Else> = IsUncapitale<L> extends true ? Then : Else;
-export type IfCapitale<L extends string, Then, Else> = IsCapitale<L> extends true ? Then : Else;
+export type IfUncapitalized<L extends string, Then, Else> = IsUncapitalized<L> extends true
+	? Then
+	: Else;
+export type IfCapitalized<L extends string, Then, Else> = IsCapitalized<L> extends true
+	? Then
+	: Else;
 export type IfUpper<L extends string, Then, Else> = IsUpper<L> extends true ? Then : Else;
 export type IfLower<L extends string, Then, Else> = IsLower<L> extends true ? Then : Else;
 export type IfEmpty<L extends string, Then, Else> = IsEmpty<L> extends true ? Then : Else;
@@ -24,29 +28,29 @@ export type WithoutSplitters<
 	: '';
 
 export type CamelCase<S extends string> = WithoutSplitters<LowerSnakeCase<S>>;
-export type PascalCase<S extends string> = Capitalize<CamelCase>;
+export type PascalCase<S extends string> = Capitalize<CamelCase<S>>;
 
 export type LowerSnakeCase<S extends string> =
 	WithoutSplitters<S> extends `${infer H}${infer B}${infer T}`
-		? IfUncapitale<
+		? IfUncapitalized<
 				H,
-				IfUncapitale<
+				IfUncapitalized<
 					B,
-					IfUncapitale<
+					IfUncapitalized<
 						T,
 						`${H}${B}${LowerSnakeCase<T>}`, // hbt...
 						`${H}${B}_${LowerSnakeCase<T>}` // hbT...
 					>,
 					`${H}_${LowerSnakeCase<`${B}${T}`>}` // hBt | hBT...
 				>,
-				IfUncapitale<
+				IfUncapitalized<
 					B,
-					IfUncapitale<
+					IfUncapitalized<
 						T,
 						`${Lowercase<H>}${B}${LowerSnakeCase<T>}`, // Hbt...
 						`${Lowercase<H>}${B}_${LowerSnakeCase<T>}` // HbT...
 					>,
-					IfUncapitale<
+					IfUncapitalized<
 						T,
 						IfEmpty<
 							T,
