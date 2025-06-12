9d0810c104cc8142b414c9d74c882d4142811ad8
P-PRINTER <psevdoprinter@gmail.com>
2025-06-12 09:33:07 +0200

    +Update connectHooks and ConnectionProvider appropriate refactored Connection when making it entity also

--- src/hooks/connectHooks.ts
+++ src/hooks/connectHooks.ts
@@ -1,57 +1,46 @@
-import { Listener } from "@lib/pprinter-tools";
-import { Peer, PeerEventMap } from "@lib/webrtc";
-import { Signal } from "@api/Signal";
-import { ConnectionContext, ConnectionCValue } from "@store/ConnectionProvider";
-import { useContext, useEffect, useState } from "react";
+import { Listener } from '@lib/pprinter-tools';
+import { Peer, PeerEventMap } from '@entities/Peer';
+import { ConnectionContext, ConnectionCValue } from '@store/ConnectionProvider';
+import { useContext, useEffect, useState } from 'react';
 
-
-export const useConnection = (): ConnectionCValue => { return getConnectionContext() };
+export const useConnection = (): ConnectionCValue => {
+	return getConnectionContext();
+};
 
 export const usePeer = (): Peer => {
-
-    const [connection] = getConnectionContext();
-    return connection.peer;
+	const [connection] = getConnectionContext();
+	return connection.getPeer();
 };
 
-export const useSignal = (): Signal => {
+export const usePeerState = (): RTCPeerConnection['connectionState'] => {
+	const peer: Peer = usePeer();
+	const [peerState, setPeerState] = useState<RTCPeerConnection['connectionState']>(peer.getState());
 
-    const [connection] = getConnectionContext();
-    return connection.signal;
+	useEffect(() => {
+		setPeerState(peer.getState());
+
+		const updatePeerState: Listener<PeerEventMap['updated']> = ({ state }) => {
+			setPeerState(state);
+		};
+
+		peer.on('updated', updatePeerState);
+		return () => peer.off('updated', updatePeerState);
+	}, [peer]);
+
+	return peerState;
 };
-
-
-export const usePeerState = (): RTCPeerConnection["connectionState"] => {
-
-    const peer: Peer                = usePeer();
-    const [peerState, setPeerState] = useState<RTCPeerConnection["connectionState"]>(peer.getState());
-
-    useEffect(() => {
-        setPeerState( peer.getState() );
-        
-        const updatePeerState: Listener<PeerEventMap['updated']> = ({ state }) => {
-            setPeerState(state);
-        };
-
-        peer.on("updated", updatePeerState);
-        return () => peer.off("updated", updatePeerState);
-    }, [peer]);
-
-    return peerState;
-};
-
 
 // === HELPERS ===
-function getConnectionContext (hookName?: string): ConnectionCValue {
+function getConnectionContext(hookName?: string): ConnectionCValue {
+	// === SUCCESS ===
+	const context: ConnectionCValue | null = useContext(ConnectionContext);
+	if (context) return context;
 
-    // === SUCCESS ===
-    const context: ConnectionCValue | null = useContext(ConnectionContext);
-    if (context) return context;
+	// === FAIL ===
+	let errMsg: string = '';
 
-    // === FAIL ===
-    let errMsg: string = "";
+	if (hookName) errMsg = `${hookName} must be used within a ConnectionProvider`;
+	else errMsg = 'useContext(ConnectionContext) should be called within the ConnectionProvider';
 
-    if (hookName)   errMsg = `${hookName} must be used within a ConnectionProvider`;
-    else            errMsg = "useContext(ConnectionContext) should be called within the ConnectionProvider";
-
-    throw new Error(errMsg);
-};
+	throw new Error(errMsg);
+}
--- src/store/ConnectionProvider.tsx
+++ src/store/ConnectionProvider.tsx
@@ -1,24 +1,21 @@
-import { Connection } from "@services/Connection";
-import { createContext, PropsWithChildren, useRef, useState } from "react";
-
+import { Connection } from 'entities/Connection';
+import { createContext, PropsWithChildren, useRef, useState } from 'react';
 
 export type UpdateConnection = () => void;
 export type ConnectionCValue = [Connection, UpdateConnection];
 
-export const ConnectionContext  = createContext<ConnectionCValue | null>(null);
-
+export const ConnectionContext = createContext<ConnectionCValue | null>(null);
 
 export const ConnectionProvider: React.FC<PropsWithChildren> = ({ children }) => {
+	const [connection, setConnection] = useState(Connection());
+	const updateConnectionRef = useRef<UpdateConnection>(() => {
+		connection.close();
+		setConnection(Connection());
+	});
 
-    const [connection, setConnection]   = useState( Connection() );
-    const updateConnectionRef           = useRef<UpdateConnection>(() => {
-        connection.destroy();
-        setConnection( Connection() );
-    });
-
-    return (
-        <ConnectionContext.Provider value={[connection, updateConnectionRef.current]}>
-            {children}
-        </ConnectionContext.Provider>
-    );
+	return (
+		<ConnectionContext.Provider value={[connection, updateConnectionRef.current]}>
+			{children}
+		</ConnectionContext.Provider>
+	);
 };
