a6b586022da8a77c4f9ca7e5ba206dd4de3eacb0
P-PRINTER <psevdoprinter@gmail.com>
2025-06-16 13:13:04 +0200

    ~Replace bridges ad entity helpers to `entitities/helprts.ts` ~`ChatPeerBus` -> `chatPeerBridge` and refactoring ~`signalPeerBus` -> `configurePeer` and delete from services

---
+++ src/entities/helpers.ts
@@ -0,0 +1,85 @@
+import { Peer, PEER_EVENTS, PeerEventMap } from '../lib/webrtc/Peer';
+import { CHAT_HISTORY_EVENTS, ChatHistory, ChatHistoryEventMap } from '@lib/chat-history';
+import { addDebug, listen, ListenerCollection, unlisten } from '@lib/utils';
+import { ISignal, Signal } from './Signal';
+import { ActionMap, ACTIONS } from '@api/socket-api';
+import { Session } from './Session';
+
+export const CHAT_NAME = 'CHAT';
+
+export type Destroy = () => void;
+
+export function chatPeerBridge(chat: ChatHistory, peer: Peer): Destroy {
+	peer.addDataChannel(CHAT_NAME);
+
+	const peerListeners: ListenerCollection<PeerEventMap> = {
+		[PEER_EVENTS.TEXT]: ({ data, label }) => {
+			if (label !== CHAT_NAME) return;
+			if (Session.target === null) throw Error('[Session.target] is not set.');
+			chat.add(data, Session.target);
+		},
+		[PEER_EVENTS.MEDIA]: ({ media }) => {
+			if (Session.target === null) throw Error('[Session.target] is not set.');
+			Session.room.userMap.get(Session.target)?.setMedia(media);
+			addDebug('remoteMedia', media);
+		},
+	};
+
+	const historyListeners: ListenerCollection<ChatHistoryEventMap> = {
+		[CHAT_HISTORY_EVENTS.ADD]: ({ item: { chatter, text } }) => {
+			if (chatter !== Session.client) return;
+			peer.send(text, CHAT_NAME);
+		},
+	};
+
+	listen<Peer, PeerEventMap>(peer, peerListeners);
+	listen<ChatHistory, ChatHistoryEventMap>(chat, historyListeners);
+
+	return () => {
+		unlisten<Peer, PeerEventMap>(peer, peerListeners);
+		unlisten<ChatHistory, ChatHistoryEventMap>(chat, historyListeners);
+	};
+}
+
+export function configurePeer(peer: Peer, target: string): Destroy {
+	// === LISTENERS
+	const signalListeners: ListenerCollection<ActionMap> = {
+		[ACTIONS.ACCEPT_TARGET]: ({ target: id, offer }) => {
+			if (target !== id) return;
+			offer && peer.start();
+		},
+		[ACTIONS.ACCEPT_SDP]: ({ target: id, sdp }) => {
+			if (target !== id) return;
+			peer.setRemoteSdp(sdp);
+		},
+		[ACTIONS.ACCEPT_ICE]: ({ target: id, ice }) => {
+			if (target !== id) return;
+			peer.addCandidate(ice);
+		},
+	};
+
+	const peerListeners: ListenerCollection<PeerEventMap> = {
+		[PEER_EVENTS.SDP]: ({ sdp }) => {
+			Signal.exec(ACTIONS.RELAY_SDP, { target, sdp });
+		},
+		[PEER_EVENTS.ICE]: ({ candidate }) => {
+			Signal.exec(ACTIONS.RELAY_ICE, { target, ice: candidate });
+		},
+	};
+
+	// === HELPERS ===
+	const listenSignal = (): void => listen<ISignal, ActionMap>(Signal, signalListeners);
+	const unlistenSignal = (): void => unlisten<ISignal, ActionMap>(Signal, signalListeners);
+
+	const listenPeer = (): void => listen<Peer, PeerEventMap>(peer, peerListeners);
+	const unlistenPeer = (): void => unlisten<Peer, PeerEventMap>(peer, peerListeners);
+
+	// === EXEC ===
+	listenPeer();
+	listenSignal();
+
+	return () => {
+		unlistenPeer();
+		unlistenSignal();
+	};
+}
