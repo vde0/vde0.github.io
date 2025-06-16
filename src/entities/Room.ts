806791d9ed422024d17f4976097e5038e3efd8ed
P-PRINTER <psevdoprinter@gmail.com>
2025-06-16 13:15:16 +0200

    -Delete `DuoChatUnit` entity -Delete any `Connection` and `DuoChatUnit` links from Room

--- src/entities/Room.ts
+++ src/entities/Room.ts
@@ -1,19 +1,30 @@
-import { Connection } from './Connection';
-import { DuoChatUnit } from './DuoChatUnit';
+import { ChatHistory } from '@lib/chat-history';
+import { IUser, User } from './User';
 
 export interface IRoom {
-	chatUnit: DuoChatUnit;
-	connection: Connection;
+	chat: ChatHistory;
+	userMap: Map<string, IUser<string>>;
+	addUser<ID extends string>(id: ID): IUser<ID>;
+	delUser<ID extends string>(id: ID): boolean;
 }
 
 type RoomConstructor = new () => IRoom;
 
 export const Room: RoomConstructor = function (): IRoom {
-	const chatUnit = new DuoChatUnit();
-	const connection = new Connection();
+	const chat: IRoom['chat'] = new ChatHistory();
+	const userMap: IRoom['userMap'] = new Map();
+
+	const addUser: IRoom['addUser'] = function (id) {
+		return new User(id);
+	};
+	const delUser: IRoom['delUser'] = function (id) {
+		return userMap.delete(id);
+	};
 
 	return {
-		chatUnit,
-		connection,
+		chat,
+		userMap,
+		addUser,
+		delUser,
 	};
 } as unknown as RoomConstructor;
