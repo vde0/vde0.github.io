8406714d83853e916372b121b5f7a9674f4e3749
P-PRINTER <psevdoprinter@gmail.com>
2025-06-17 12:52:07 +0200

    == ROOM == ~Fix `addUser` method: push `User` into `userMap` field map -Delete `userMap` as public field (save as private yet) +Add `hasUser`: boolean method

--- src/entities/Room.ts
+++ src/entities/Room.ts
@@ -3,20 +3,22 @@ import { IUser, User, UserId } from './User';
 
 export interface IRoom {
 	chat: IChat;
-	userMap: Map<UserId, IUser<UserId>>;
 	addUser<ID extends UserId>(id: ID): IUser<ID>;
 	delUser<ID extends UserId>(id: ID): boolean;
 	getUser<ID extends UserId>(id: ID): IUser<ID> | undefined;
+	hasUser<ID extends UserId>(id: ID): boolean;
 }
 
 type RoomConstructor = new () => IRoom;
 
 export const Room: RoomConstructor = function (): IRoom {
 	const chat: IRoom['chat'] = new Chat();
-	const userMap: IRoom['userMap'] = new Map();
+	const userMap: Map<UserId, IUser<UserId>> = new Map();
 
 	const addUser: IRoom['addUser'] = function (id) {
-		return new User(id);
+		const user = new User(id);
+		userMap.set(id, user);
+		return user;
 	};
 	const delUser: IRoom['delUser'] = function (id) {
 		return userMap.delete(id);
@@ -24,12 +26,15 @@ export const Room: RoomConstructor = function (): IRoom {
 	const getUser: IRoom['getUser'] = function (id) {
 		return userMap.get(id) as IUser<typeof id> | undefined;
 	};
+	const hasUser: IRoom['hasUser'] = function (id) {
+		return userMap.has(id);
+	};
 
 	return {
 		chat,
-		userMap,
 		addUser,
 		delUser,
 		getUser,
+		hasUser,
 	};
 } as unknown as RoomConstructor;
