d83d5ba3224b04afa15e5aeed0e805067538bd07
P-PRINTER <psevdoprinter@gmail.com>
2025-06-17 11:08:28 +0200

    -Delete using of deleted `RoomProvider` from `App`

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,15 @@ import React from 'react';
 import Main from './containers/Main';
 import ChatProvider from '@store/ChatProvider';
 import { WebAppProvider } from '@vkruglikov/react-telegram-web-app';
-import { RoomProvider } from '@store/RoomProvider';
 
 const MemoMain = React.memo(Main);
 const MemoChatProvider = React.memo(ChatProvider);
-const MemoRoomProvider = React.memo(RoomProvider);
 
 const App: React.FC = () => (
 	<WebAppProvider>
-		<MemoRoomProvider>
-			<MemoChatProvider>
-				<MemoMain />
-			</MemoChatProvider>
-		</MemoRoomProvider>
+		<MemoChatProvider>
+			<MemoMain />
+		</MemoChatProvider>
 	</WebAppProvider>
 );
 
