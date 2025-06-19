94852fcbe146a6cf03e49d9c76eab5beef228e55
P-PRINTER <psevdoprinter@gmail.com>
2025-06-19 12:32:50 +0200

    == SERVICES == ~Update `init` focusing for work with intents

--- src/services/init.ts
+++ src/services/init.ts
@@ -1,23 +1,43 @@
 import '@services/signal';
 import '@services/peer';
-import { INTENT_ACTIONS, intentChest } from './intents';
+import { DO_INTENT, INTENT_ACTIONS, intentChest } from './intents';
 import { requestLocalMedia, whenLocalMedia } from '@api/localMedia';
 import { ACC_FLAGS, appAcessor } from './appAccessor';
 import { Session, SESSION_EVENTS } from '@entities/Session';
 import { getWebApp } from '@lib/utils';
+import { USER_EVENTS } from '@entities/User';
+import { Profile } from '@entities/Profile';
 
+// == INTENT LISTENERS ==
+// START_APP
 {
+	appAcessor.when(() => {
+		DO_INTENT[INTENT_ACTIONS.NEXT]();
+	});
+
 	intentChest.once(INTENT_ACTIONS.START_APP, () => {
 		requestLocalMedia();
-		whenLocalMedia((media) => media && appAcessor.set(ACC_FLAGS.LOCAL_MEDIA));
+		whenLocalMedia((media) => {
+			console.log('INIT LISTENER CLIENT MEDIA');
+			// Session.room
+			// 	.getUser(Session.client)!
+			// 	.once(USER_EVENTS.MEDIA, () => console.log('CLIENT MEDIA'));
+			Profile.user.setMedia(media);
+			appAcessor.set(ACC_FLAGS.LOCAL_MEDIA);
+		});
 	});
 }
 
+// NEXT
 {
-	appAcessor.when(() => Session.findTarget());
-	Session.on(SESSION_EVENTS.NEXT_TARGET, ({ connection }) => connection?.connect());
+	intentChest.on(INTENT_ACTIONS.NEXT, () => {
+		console.log('NEXT');
+		Profile.updateRoom();
+		Session.next();
+	});
 }
 
+// == CONFIG WEB APP
 {
 	const webApp = getWebApp();
 	if (webApp)
